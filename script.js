ab8a8887038509db0d78a54916fdf89650993d76
erysameth <alexandrasamuel2004@gmail.com>
2025-03-24 15:55:36 -0300

    add adjusts on alt image on JS

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   //pegar a tag img
 
   const img = document.querySelector("#profile img")
 
   //substituir a imagem
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     //se tiver light mode, adicionar imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
-  } else { 
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
     //se tiver sem light mode, manter imagem normal
-      img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/avatar.png")
   }
 
-
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "Selfie de Alexandra fazendo biquinho para a câmera"
+    )
+  } else {
+    img.setAttribute("alt", "Foto de Alexandra séria olhando para a câmera")
   }
+}
